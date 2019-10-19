4c388b51f289f420425622825d4847cbffdfca3d
Michał Chęciński <michal.checinski@outlook.com>
2019-10-19 11:29:28 +0200

    Change example dto

--- Pop-Grupa-A-Backend/app/main/util/dto.py
+++ Pop-Grupa-A-Backend/app/main/util/dto.py
@@ -6,6 +6,5 @@ class UserDto:
     user = api.model('user', {
         'email': fields.String(required=True, description='user email address'),
         'username': fields.String(required=True, description='user username'),
-        'password': fields.String(required=True, description='user password'),
-        'public_id': fields.String(description='user Identifier')
+        'id': fields.Integer(description='user Identifier')
     })
