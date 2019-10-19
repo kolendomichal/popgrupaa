855baedaa4658344637746fe5c17ba16c59b9661
Michał Chęciński <michal.checinski@outlook.com>
2019-10-19 11:27:51 +0200

    fix manage

--- Pop-Grupa-A-Backend/app/manage.py
+++ Pop-Grupa-A-Backend/app/manage.py
@@ -4,12 +4,14 @@ import unittest
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-from app.main import create_app, db
-from app import blueprint
-from app.main.model import user
+from main import create_app, db
+
+from main.model import ComputationAccount
+from main.model import ComputationApplication
+from main.model import ComputationTask
 
 app = create_app('dev')
-app.register_blueprint(blueprint)
+
 app.app_context().push()
 
 manager = Manager(app)
@@ -18,21 +20,18 @@ migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
 
-
 @manager.command
 def run():
     app.run()
 
-
 @manager.command
 def test():
     """Runs the unit tests."""
-    tests = unittest.TestLoader().discover('./test', pattern='test*.py')
+    tests = unittest.TestLoader().discover('test', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
     return 1
-
 
 if __name__ == '__main__':
     manager.run()
