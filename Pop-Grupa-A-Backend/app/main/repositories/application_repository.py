6b53c2d7747e3b12a8ca456a259836a27654633e
Michał Chęciński <michal.checinski@outlook.com>
2019-10-19 11:26:41 +0200

    Add repositories for models

---
+++ Pop-Grupa-A-Backend/app/main/repositories/application_repository.py
@@ -0,0 +1,11 @@
+from app.main import db
+from app.main.model.ComputationApplication import ComputationApplication
+
+
+def get_all_applications():
+    return ComputationApplication.query.query.all()
+
+
+def save_changes(data):
+    db.session.add(data)
+    db.session.commit()
