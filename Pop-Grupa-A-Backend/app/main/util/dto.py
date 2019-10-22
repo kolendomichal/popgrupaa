e441789f6e4b68af3c620f603b80d71696187ea4
Filip Choromanski (FC) <FC@kmd.dk>
2019-10-22 19:51:57 +0200

    Created DTO for Application model

--- Pop-Grupa-A-Backend/app/main/util/dto.py
+++ Pop-Grupa-A-Backend/app/main/util/dto.py
@@ -9,3 +9,12 @@ class UserDto:
         'password': fields.String(required=True, description='user password'),
         'id': fields.Integer(description='user Identifier')
     })
+
+class ApplicationDto:
+    api = Namespace('application', description='application related operations')
+    application = api.model('application', {
+        'name': fields.String(required=False, description='application name'),
+        'description': fields.String(required=False, description='application description'),
+        'icon': fields.String(required=False, description='application icon as string of bytes'),
+        'id': fields.Integer(description='application identifier')
+    })
