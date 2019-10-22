a92a755663902fb3c0d88c16a0dc55b2c6733f6b
Filip Choromanski (FC) <FC@kmd.dk>
2019-10-22 19:56:09 +0200

    Added application_controller to blueprint

--- Pop-Grupa-A-Backend/app/__init__.py
+++ Pop-Grupa-A-Backend/app/__init__.py
@@ -4,6 +4,8 @@ from flask_restplus import Api
 from flask import Blueprint
 
 from .main.controller.user_controller import api as user_ns
+from .main.controller.application_controller import api as application_ns
+
 
 blueprint = Blueprint('api', __name__)
 
@@ -14,3 +16,5 @@ api = Api(blueprint,
           )
 
 api.add_namespace(user_ns, path='/user')
+api.add_namespace(application_ns, path='/application')
+
