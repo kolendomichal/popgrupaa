de0395b5f3b493d6efc4f4a163b666a44fa61478
Filip Choromanski (FC) <FC@kmd.dk>
2019-10-23 11:02:09 +0200

    Removed comments from application_controller

--- Pop-Grupa-A-Backend/app/main/controller/application_controller.py
+++ Pop-Grupa-A-Backend/app/main/controller/application_controller.py
@@ -9,15 +9,13 @@ _application = ApplicationDto.application
 
 
 @api.route('/')
+@api.response(200, 'Success.')
+@api.response(204, 'Could not find any application.')
 class ApplicationList(Resource):
-    # @api.response(200, 'List successfully collected.')
     @api.doc('Get list of all applications')
     @api.marshal_with(_application, as_list=True)
     def get(self):
         """get application list"""
         applications_list = get_all_applications()
-        # if len(applications_list) == 0:
         status_code = 204 if len(applications_list) == 0 else 200
-        # else:
-            # status_code = 200
         return applications_list , status_code
