a8bd3f69056dc66335b89aca4e6f5d135f7c786d
annakonieczna <anna.konieczna@neuralalpha.com>
2019-10-29 19:46:11 +0100

    chan

--- Pop-Grupa-A-Backend/app/main/controller/task_controller.py
+++ Pop-Grupa-A-Backend/app/main/controller/task_controller.py
@@ -40,7 +40,7 @@ class TaskActivate(Resource):
         try:
             task = get_task_for_task_id(task_id=task_id).all()
 # NOTE: status 3 - Working
-            task = change_status_for_task(task, 3)
+            task = change_status_for_task(task, ComputationStatus.WORKING.value)
             return 200, update_task(task)
         except:
             return 404
