f67607d614607c1fedea777980d91be1963a3aec
Katarzyna Kolasa <kt.kolasa@gmail.com>
2019-10-30 11:41:11 +0100

    delete/comment out unused values

--- Pop-Grupa-A-Backend/app/main/controller/task_controller.py
+++ Pop-Grupa-A-Backend/app/main/controller/task_controller.py
@@ -1,14 +1,12 @@
 from flask import request
 from flask_restplus import Resource
-from ..util.DTO.ComputationTask.TaskDTO import ComputationTaskDto
+from ..util.DTO.ComputationTask.TaskDTO import ComputationTaskDto, StatusDto
 from ..services.task_service import *
-from flask_restplus import cors
 
 api = ComputationTaskDto.api
 _createModel = ComputationTaskDto.createModel
 _taskDTO = ComputationTaskDto.task
 _status = StatusDto.status
-
 
 @api.route('/')
 class TaskCreate(Resource):
@@ -35,11 +33,10 @@ class TaskGet(Resource):
 @api.param('task_id', 'The Task identifier')
 class TaskActivate(Resource):
     @api.doc('activates task')
-    @api.marshal_with(_task)
+    #@api.marshal_with(_task)
     def post(self, task_id):
         try:
-            task = get_task_for_task_id(task_id=task_id).all()
-# NOTE: status 3 - Working
+            task = get_task_for_task_id(id=task_id).all()
             task = change_status_for_task(task, ComputationStatus.WORKING.value)
             return 200, update_task(task)
         except:
