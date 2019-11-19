239a9bfa5d0d3d9760f0056dd3b8b1875a7f1a70
balcevia <45275966+balcevia@users.noreply.github.com>
2019-11-19 22:58:09 +0100

    Delete unnecessary files

--- Pop-Grupa-A-Backend/app/main/util/DTO/ComputationStatusDTO.py
+++
@@ -1,9 +0,0 @@
-from flask_restplus import Namespace, fields
-
-
-class ComputationStatusDto:
-    api = Namespace('task', description='status related operations')
-    status = api.model('task', {
-        'status': fields.Integer(description='task status'),
-        'message': fields.Integer(description='task message'),
-    })
--- Pop-Grupa-A-Frontend/src/app/constants.js
+++
@@ -1,9 +0,0 @@
-export const FormNames = {
-    LoginForm: 'loginForm',
-    RegistrationForm: 'registrationForm'
-};
-
-export const Status = {
-    Fail: 'fail',
-    Success: 'success'
-};
