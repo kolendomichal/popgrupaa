613a9d06aef257fc1e8a0135fd508db039964587
Bartosz Strachowski <bstrachowski@gmail.com>
2019-12-03 21:48:19 +0100

    navbars switched to arrow funcs

--- Pop-Grupa-A-Frontend/src/components/navigationBar/NavBars.jsx
+++ Pop-Grupa-A-Frontend/src/components/navigationBar/NavBars.jsx
@@ -4,47 +4,36 @@ import Nav from 'react-bootstrap/Nav'
 import 'bootstrap/dist/css/bootstrap.css';
 
 
-function LoginRegistrationNavBar() {
-    return (
-        <Navbar bg="light" variant="light" className="mb-4">
-            <Navbar.Brand >BalticLSC</Navbar.Brand>
-        </Navbar>
-    );
-}
-
-function ComputationCockpitNavBar() {
-    return (
-        <Navbar bg="light" variant="light" className="mb-4">
-            <Navbar.Brand >BalticLSC</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link >Computation Cockpit</Nav.Link>
-            </Nav>
-            <Nav>
-                <Nav.Link eventKey={2} href="#logout">
-                    Log Out
-                </Nav.Link></Nav>
-        </Navbar>
-    );
-}
-
-function ComputationResourceNavBar() {
-    return (
-        <Navbar bg="light" variant="light" className="mb-4">
-            <Navbar.Brand >BalticLSC</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="/computation-resource-management">Computation Resource Management</Nav.Link>
-            </Nav>
-            <Nav>
-                <Nav.Link eventKey={2} href="#logout">
-                    Log Out
-                </Nav.Link></Nav>
-        </Navbar>
-    );
-}
+export const LoginRegistrationNavBar = () => (
+    <Navbar bg="light" variant="light" className="mb-4">
+        <Navbar.Brand >BalticLSC</Navbar.Brand>
+    </Navbar>
+);
 
 
-export {
-    LoginRegistrationNavBar,
-    ComputationCockpitNavBar,
-    ComputationResourceNavBar
-};
+export const ComputationCockpitNavBar = () => (
+    <Navbar bg="light" variant="light" className="mb-4">
+        <Navbar.Brand >BalticLSC</Navbar.Brand>
+        <Nav className="mr-auto">
+            <Nav.Link >Computation Cockpit</Nav.Link>
+        </Nav>
+        <Nav>
+            <Nav.Link eventKey={2} href="#logout">
+                Log Out
+            </Nav.Link></Nav>
+    </Navbar>
+);
+
+
+export const ComputationResourceNavBar = () => (
+    <Navbar bg="light" variant="light" className="mb-4">
+        <Navbar.Brand >BalticLSC</Navbar.Brand>
+        <Nav className="mr-auto">
+            <Nav.Link href="/computation-resource-management">Computation Resource Management</Nav.Link>
+        </Nav>  
+        <Nav>
+            <Nav.Link eventKey={2} href="#logout">
+                Log Out
+            </Nav.Link></Nav>
+    </Navbar>
+);
