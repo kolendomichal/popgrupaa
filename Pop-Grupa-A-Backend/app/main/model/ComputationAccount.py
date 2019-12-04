5428c5c36e512b4878e66e342f069ae26eb3c9f1
krolakb <krolakbartlomiej@gmail.com>
2019-12-04 18:09:24 +0100

    Add role field in registration

--- Pop-Grupa-A-Backend/app/main/model/ComputationAccount.py
+++ Pop-Grupa-A-Backend/app/main/model/ComputationAccount.py
@@ -1,5 +1,5 @@
 from .. import db, flask_bcrypt
-
+from app.main.model.AccountRole import AccountRole
 
 class ComputationAccount(db.Model):
     __tablename__ = "ComputationAccount"
@@ -10,6 +10,7 @@ class ComputationAccount(db.Model):
     lastLogin = db.Column(db.DateTime, nullable=False)
     username = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(100))
+    role = db.Column(db.Enum(AccountRole), unique=False, nullable=False)
 
     def __repr__(self):
         return "<User '{}'>".format(self.username)
