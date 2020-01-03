9746410b54e686b1e9aa9f3ddccb7e6ff4958c8a
Jakub Rejent (JFF) <JFF@kmd.dk>
2020-01-03 17:58:23 +0100

    Create endpoints for: get machine load, get task info, task create, task activate

--- machine_flask_api.py
+++ machine_flask_api.py
@@ -23,6 +23,32 @@ def get_machine_data():
                     
     return jsonify({ "CPU": CPU, "GPU": GPU, "IP_ADDRESS": ip_address + ':' + str(port), "CONTAINER_IP_ADDRESS": container_ip + ':' + str(port)})
 
+@app.route("/get-machine-task-info", methods=['GET'])
+def get_macine_task_info():
+    # sprawdz czy jakies zadanie chodzi na maszynie
+    is_free = True
+    if is_free:
+        return jsonify({"status":"FREE", "message":"No tasks are running on machine."})
+    else:
+        return jsonify({"status":"BUSY", "message":"At least one task is currently running on machine."})
+ 
+@app.route("/get-machine-load-info", methods=['GET'])
+def get_machine_load_info():
+    CPU_Pct=str(round(float(os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline()),2))
+    return jsonify({"load_percent":CPU_Pct})
+
+@app.route("/task/<id>/create")
+def create_new_task(id):
+    # pobierz aplikacje za pomocą IStorage
+    # zapisz aplikcaje na dysku
+    # trzeba jeszcze uwzglednić id taska
+    return jsonify("Task created")
+ 
+@app.route("/task/<id>/activate")
+def activate_task():
+    # uruchomic wczesniej zapisany task
+    return jsonify("Task activted")
+
 @app.route("/verify/<number>", methods=['GET'])
 def get_binary_string(number):
     return bin(int(number))
