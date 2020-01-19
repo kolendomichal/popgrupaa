80830a3968e2b8cc2fdc21b44b5ce8c64d994b1c
wasikm04 <maciej.wasik1@gmail.com>
2020-01-19 14:27:50 +0100

    removed unused exchange layer

--- Pop-Grupa-A-Scheduler/scheduler.py
+++ Pop-Grupa-A-Scheduler/scheduler.py
@@ -3,37 +3,16 @@ import json
 from app.helper import fetchMachines
 from app.algorithm.algorithmManager import AlgorithmManager
 
-EXCHANGE = "task_assign"
 params = pika.URLParameters(os.environ.get("RABBIT_URL"))
 
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
 channel.queue_declare(queue="ActivateComputationTask", durable=True)
-channel.exchange_declare(exchange=EXCHANGE, exchange_type="direct")
 
-
-# For testing purposes
-# def test():
-#     for i in range(0,2):
-#         message = i
-#         channel.queue_declare(queue="machine_12", durable=True)
-#         channel.basic_publish(exchange=EXCHANGE,
-#                               routing_key='machine_12',
-#                               body=str(message))
-#     for i in range(13,20):
-#         message = i
-#         channel.queue_declare(queue="machine_{}".format(str(i)), durable=True)
-#         channel.basic_publish(exchange=EXCHANGE,
-#                               routing_key='machine_{}'.format(str(i)),
-#                               body=str(message))
-#
-# test()
 
 machines = fetchMachines()
 algorithmManager = AlgorithmManager(machines)
 print(" [*] Waiting for messages from ActivateComputationTask")
-
-
 
 
 def consumeNewTask(ch, method, properties, body):
@@ -48,7 +27,7 @@ def consumeNewTask(ch, method, properties, body):
 def sendTaskToChoosenMachine(chosenMachine, messageBody):
     print("Sending {} to machine with ID = {}".format(messageBody, chosenMachine))
     channel.basic_publish(
-        exchange=EXCHANGE,
+        exchange='',
         routing_key='machine_{}'.format(chosenMachine),
         body=json.dumps(messageBody),
         properties=pika.BasicProperties(delivery_mode=2,)
