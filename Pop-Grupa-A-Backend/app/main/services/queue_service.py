7d03df772a0e8490b03e47c8a326852d4a886538
Piotr <bodzio_chomik@wp.pl>
2020-01-21 22:27:45 +0100

    Poprawione metody

--- Pop-Grupa-A-Backend/app/main/services/queue_service.py
+++ Pop-Grupa-A-Backend/app/main/services/queue_service.py
@@ -2,15 +2,17 @@ import pika
 import json
 
 def push_machine_task(machine_id, task_id, application_id):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    params = pika.URLParameters('amqp://testuser:testpassword@172.17.0.1:5672/test-vhost')
+    connection = pika.BlockingConnection(params)
     channel = connection.channel()
-    queue_id = 'machine_' + machine_id
+    queue_id = 'machine_' + str(machine_id)
     body = json.dumps({"task_id": task_id, "application_id": application_id})
     channel.basic_publish(exchange='', routing_key=queue_id, body=body)
     connection.close()
 
 def push_unassigned_task(task_id, application_id):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    params = pika.URLParameters('amqp://testuser:testpassword@172.17.0.1:5672/test-vhost')
+    connection = pika.BlockingConnection(params)
     channel = connection.channel()
     channel.queue_declare(queue='activate_computation_task')
     body = json.dumps({"task_id": task_id, "application_id": application_id})
@@ -18,43 +20,44 @@ def push_unassigned_task(task_id, application_id):
     connection.close()
 
 def push_task_status(task_status, task_id, reason):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    params = pika.URLParameters('amqp://testuser:testpassword@172.17.0.1:5672/test-vhost')
+    connection = pika.BlockingConnection(params)
     channel = connection.channel()
     channel.queue_declare(queue='queue_task_status')
-    body = json.dumps({"task_status": task_status ,"task_id": task_id, "reason": reason})
+    body = json.dumps({"task_status": task_status,"task_id": task_id, "reason": reason})
     channel.basic_publish(exchange='', routing_key='queue_task_status', body=body)
     connection.close()
 
 def pop_machine_task(machine_id):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    params = pika.URLParameters('amqp://testuser:testpassword@172.17.0.1:5672/test-vhost')
+    connection = pika.BlockingConnection(params)
     channel = connection.channel()
-    queue_id = 'machine_' + machine_id
+    queue_id = 'machine_' + str(machine_id)
     channel.queue_declare(queue=queue_id)
     method_frame, header_frame, body = channel.basic_get(queue_id)
     if method_frame:
-        print(method_frame, header_frame, body)
         channel.basic_ack(method_frame.delivery_tag)
     connection.close()
     return body
 
 def pop_unassigned_task():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    params = pika.URLParameters('amqp://testuser:testpassword@172.17.0.1:5672/test-vhost')
+    connection = pika.BlockingConnection(params)
     channel = connection.channel()
     channel.queue_declare(queue='activate_computation_task')
     method_frame, header_frame, body = channel.basic_get('activate_computation_task')
     if method_frame:
-        print(method_frame, header_frame, body)
         channel.basic_ack(method_frame.delivery_tag)
     connection.close()
     return body
 
 def pop_taks_status():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    params = pika.URLParameters('amqp://testuser:testpassword@172.17.0.1:5672/test-vhost')
+    connection = pika.BlockingConnection(params)
     channel = connection.channel()
     channel.queue_declare(queue='queue_task_status')
     method_frame, header_frame, body = channel.basic_get('queue_task_status')
     if method_frame:
-        print(method_frame, header_frame, body)
         channel.basic_ack(method_frame.delivery_tag)
     connection.close()
     return body
