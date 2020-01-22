2c19e369916f7c450109ea0d9839fb223cd3d8f0
Bartosz Strachowski <bstrachowski@gmail.com>
2020-01-22 09:16:43 +0100

    move credentials to env, install pika and use json dump

--- Pop-Grupa-A-Backend/app/main/services/task_service.py
+++ Pop-Grupa-A-Backend/app/main/services/task_service.py
@@ -1,6 +1,7 @@
 import pika
 import os
 from flask import request, abort
+import json
 
 import app.main.repositories.task_repository as task_repository
 import app.main.repositories.user_repository as user_repository
@@ -35,24 +36,21 @@ def add_task(task):
 
 
 def send_message_to_scheduler(task_id):
-    #todo verify the form of ActivateComputationTaskMessage- is it a class defined somewhere
-    #todo verify connection parameters
     app_id = (task_repository.get_task_for_task_id(task_id)).app_id
     body = {
         'app_id': app_id,
         'task_id': task_id
     }
-    url = os.environ.get('CLOUDAMQP_URL',
-                         'amqp://rabbitmq:rabbitmq@172.18.0.6:5672/%2f')  # adres się zmienia, ja miałem 172.18.0.5 więc trzeba to do zmiennej wrzucić, pewnie ta sama sytuacja jest też z adresem backendu
-    params = pika.URLParameters(url)
+  
+    params = pika.URLParameters(os.environ.get("RABBIT_URL"))
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
     channel.queue_declare(queue='ActivateComputationTask', durable=True)
-    #todo queue name: ActivateComputationTask
+
     channel.basic_publish(
         exchange='',
         routing_key='ActivateComputationTask',
-        body=body,
+        body=json.dumps(body),
         properties=pika.BasicProperties(
             delivery_mode=2,  # make message persistent
         ))
@@ -64,8 +62,8 @@ def activate_task(task_id):
     try:
         task_status = task_repository.get_status(task_id)
         if (task_status == ComputationStatus.SUBMITTED):
-            task = task_repository.change_task_status(task_id, ComputationStatus.ACTIVATED)
             send_message_to_scheduler(task_id)
+            task = task_repository.change_task_status(task_id, ComputationStatus.ACTIVATED)
             response_object = {
                 'status': 'Success',
                 'message': 'Task successfuly activated.'
